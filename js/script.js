e829c74fc1271b91f030d853667df0d9e1d381a8
hunter547 <hunter547@gmail.com>
2019-10-14 16:28:14 -0400

    Updated the writing of the pokemon arrays to a fuction
    Created a function so that multiple pokemon arrays can be written to the document.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,60 @@
 var repository = [ 
-	{ 
-		name: 'Pikachu',
-		height: '0.4 m', 
-		type: 'Electric'
-	}, 
-	{
-		name: 'Blastoise', 
-		height: '1.6 m', 
-		type: 'Water'
-	}, 
-	{
-		name: 'Poliwhirl', 
-		height: '1.0 m', 
-		type: 'Water'
-	} 
-];	  
+{ 
+	name: "Pikachu",
+	height: "0.4 m", 
+	type: ["electric", "fire"]
+}, 
+{
+	name: "Blastoise", 
+	height: "1.6 m", 
+	type: "water"
+}, 
+{
+	name: "Poliwhirl", 
+	height: "1.0 m", 
+	type: ["water", "fire"]
+} 
+];	
 
-for (var i = 0; i <= repository.length; i++) {
-	if (parseFloat(repository[i].height) > 1.0) {
-		document.write('<br />' + repository[i].name+' (height: '+repository[i].height+'). Wow that is tall!');
+var repository2 = [ 
+{
+	name: "Ivysaur", 
+	height: "1.0 m", 
+	type: ["grass", "poison"]
+}, 
+{
+	name: "Gyarados", 
+	height: "6.5 m", 
+	type: ["water", "flying"]
+}, 
+{
+	name: "Genger",
+	height: "1.5 m", 
+	type: ["ghost", "poison"]
+}	
+];
+
+function printArrayDetails(pokemonList) { 
+	var displayString = "";  
+	alert(pokemonList.length + ' in this array.');
+	for (var i = 0; i <= pokemonList.length -1; i++) { 
+		alert(i);
+		alert(pokemonList[i]); 
+		displayString = "<br />" + pokemonList[i].name+" (height: "+pokemonList[i].height+")."; 
+		if (parseFloat(pokemonList[i].height) > 1.0) {
+			displayString += " Wow that is tall!"; 
+		} 
+		document.write(displayString);
 	} 
-	else {
-		document.write('<br />' + repository[i].name+' (height: '+repository[i].height+').');
+}; 
+
+function getTypes(pokemon) { 
+	if (pokemon.type == []) {
+		for (var i = 0; i <= pokemon.length; i++) {
+			
+		}
 	}
-
 };
+
+printArrayDetails(repository); 
+printArrayDetails(repository2);
