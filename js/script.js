5c86146ccd0c43a4dae05b3365d38cfc4e468da0
Hunter Evanoff <hunter547@gmail.com>
2019-10-13 20:21:52 -0400

    Created an array that stores Pokémon and made sure it prints
    Created a repository that stores Pokémon and confirmed they could be printed.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-alert('Hello World'); 
-var favoriteFood = 'Beef Stroganoff'; 
-document.write(favoriteFood);
+var repository = [ 
+	{ 
+		name: 'Pikachu',
+		hieght: '0.4 m', 
+		type: 'Electric'
+	}, 
+	{
+		name: 'Blastoise', 
+		hieght: '1.6 m', 
+		type: 'Water'
+	}, 
+	{
+		name: 'Poliwhirl', 
+		hieght: '1.0 m', 
+		type: 'Water'
+	} 
+];	  
+
+for (var i = 0; i <= repository.length; i++) {
+	 document.write(repository[i].name + '<br />'); 
+	 document.write(repository[i].hieght + '<br />'); 
+	 document.write(repository[i].type + '<br />');
+};
