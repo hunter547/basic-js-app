8c440af99374db40c1134623a43e6344e43cb5e2
Hunter Evanoff <hunter547@gmail.com>
2019-10-15 20:15:04 -0400

    Created functions to print pokemon in formatted fashion
    Wrote functions that automated the printing of the objects in a formatted fashion.

--- js/script.js
+++ js/script.js
@@ -1,60 +1,67 @@
 var repository = [ 
 { 
-	name: "Pikachu",
-	height: "0.4 m", 
-	type: ["electric", "fire"]
+	name: 'Pikachu',
+	height: '0.4 m', 
+	type: 'electric'
 }, 
 {
-	name: "Blastoise", 
-	height: "1.6 m", 
-	type: "water"
+	name: 'Blastoise', 
+	height: '1.6 m', 
+	type: 'water'
 }, 
 {
-	name: "Poliwhirl", 
-	height: "1.0 m", 
-	type: ["water", "fire"]
+	name: 'Poliwhirl', 
+	height: '1.0 m', 
+	type: 'water'
 } 
 ];	
 
 var repository2 = [ 
 {
-	name: "Ivysaur", 
-	height: "1.0 m", 
-	type: ["grass", "poison"]
+	name: 'Ivysaur', 
+	height: '1.0 m', 
+	type: ['grass', 'poison']
 }, 
 {
-	name: "Gyarados", 
-	height: "6.5 m", 
-	type: ["water", "flying"]
+	name: 'Gyarados', 
+	height: '6.5 m', 
+	type: ['water', 'flying']
 }, 
 {
-	name: "Genger",
-	height: "1.5 m", 
-	type: ["ghost", "poison"]
+	name: 'Genger',
+	height: '1.5 m', 
+	type: ['ghost', 'poison']
 }	
-];
+]; 
 
-function printArrayDetails(pokemonList) { 
-	var displayString = "";  
-	alert(pokemonList.length + ' in this array.');
-	for (var i = 0; i <= pokemonList.length -1; i++) { 
-		alert(i);
-		alert(pokemonList[i]); 
-		displayString = "<br />" + pokemonList[i].name+" (height: "+pokemonList[i].height+")."; 
-		if (parseFloat(pokemonList[i].height) > 1.0) {
-			displayString += " Wow that is tall!"; 
-		} 
-		document.write(displayString);
+var fullRepository = repository.concat(repository2);
+
+fullRepository.forEach((currentPokemon) => { 
+	printArrayDetails(currentPokemon);
+});
+
+function printArrayDetails(pokemon) { 
+	var displayString = '';  
+	displayString = '<br />' + pokemon.name+'. '+getTypes(pokemon)+' Height: '+pokemon.height; 
+	if (parseFloat(pokemon.height) > 1.0) {
+		displayString += ' - Wow, that is tall!'; 
 	} 
+	document.write(displayString);
 }; 
 
 function getTypes(pokemon) { 
-	if (pokemon.type == []) {
-		for (var i = 0; i <= pokemon.length; i++) {
-			
-		}
-	}
+	var typeString = 'Type: ';
+	if (pokemon.type instanceof Array) {
+		pokemon.type.forEach((currentType) => {
+			typeString += currentType + ' and '; 
+		});
+		typeString = typeString.substring(0,typeString.length-5) + '.';
+	} 
+	else { 
+		typeString += pokemon.type+'.';
+	} 
+	return typeString;
 };
 
-printArrayDetails(repository); 
-printArrayDetails(repository2);
+//printArrayDetails(repository); 
+//printArrayDetails(repository2);
