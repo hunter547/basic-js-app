787ece52d6b8b374c9dc3698d356f3ce4cff279b
hunter547 <hunter547@gmail.com>
2019-10-18 16:29:56 -0400

    Working on a feature that allows for users to add pokemon
    Adding more functionality by adding the ability for users to enter thier own pokemon. Still working out some kinks.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// An Immediately Invoked Function Expression to instantiate a pokemon repository and 
+// An Immediately Invoked Function Expression to instantiate a pokemon repository and protect it from being accessed globally
 var pokemonRepository = (function () {
 	var repository = [ 
 	{  	 
@@ -34,20 +34,28 @@ var pokemonRepository = (function () {
 	];
 
 	function add(pokemon) {
-		repository.push(pokemon);
+		repository.push(pokemon); // Add new pokemon to the repository
 	}
 
 	function getAll() {
-		return repository;
+		return repository; // Return all pokemon in the repository
 	} 
 
-	function printAll(poke) {
-
+	function printAll() { // Print all pokemon in the repository
+		repository.forEach((pokemon) => { 
+			var displayString = '';  
+			displayString = '<br />' + pokemon.name+'. '+getTypes(pokemon)+' Height: '+pokemon.height+'.'; 
+			if (parseFloat(pokemon.height) > 1.0) {
+				displayString += ' - Wow, that is tall!'; 
+			} 
+			document.write(displayString);
+		});
 	}
 
 	return {
 		add: add,
-		getAll: getAll
+		getAll: getAll, 
+		printAll: printAll
 	};
 })();
 
@@ -63,13 +71,13 @@ pokemonRepository.add(newPokemon);
 
 fullRepository.forEach((currentPokemon) => { 
 	printArrayDetails(currentPokemon);
-});
+}); 
 
 function printArrayDetails(pokemon) { 
 	var displayString = '';  
-	displayString = '<br />' + pokemon.name+'. '+getTypes(pokemon)+' Height: '+pokemon.height; 
+	displayString = '<br />' + pokemon.name+'. '+getTypes(pokemon)+' Height: '+pokemon.height+'.'; 
 	if (parseFloat(pokemon.height) > 1.0) {
-		displayString += ' - Wow, that is tall!'; 
+		displayString = displayString.substring(0, displayString.length-1) + ' - Wow, that is tall!'; 
 	} 
 	document.write(displayString);
 }; 
@@ -86,4 +94,165 @@ function getTypes(pokemon) {
 		typeString += pokemon.type+'.';
 	} 
 	return typeString;
-};
+};  
+
+
+function enterPokemon(){ 
+	document.body.innerHTML = '';
+	var pokemonName = enterPokemonName(); 
+	if (pokemonName == null) {
+		return;
+	}
+	var pokemonHeight = enterPokemonHeight(); 
+	if (pokemonHeight == null) {
+		return;
+	}
+	var pokemonType = enterPokemonType(); 
+	if (pokemonType == null) {
+		return;
+	}
+	var newPokemon = { 
+		name: null, 
+		height: null, 
+		type: [null]
+	};  
+
+	newPokemon.name = pokemonName; 
+	newPokemon.height = pokemonHeight; 
+	anotherType(newPokemon, pokemonType); 
+
+	pokemonRepository.add(newPokemon);
+
+	pokemonRepository.printAll(); 
+
+	var addButton = document.createElement('BUTTON'); 
+	var buttonText = document.createTextNode('Add another pokemon');  
+	addButton.onclick = function() {
+		enterPokemon();
+	}
+	addButton.appendChild(buttonText); 
+	document.writeln();  
+	document.body.appendChild(addButton);
+}; 
+
+function enterPokemonName() { 
+	var pokemonName = null; 
+	while (pokemonName == null || pokemonName == '' || typeof pokemonName != 'string' || pokemonName.indexOf(' ') != -1 || pokemonName.match(/[0-9]/) != null) { 
+		if (pokemonName == null) {
+			pokemonName = prompt('Please enter a name for your pokemon', ''); 
+			if (pokemonName == null) {
+				return;
+			}
+		}
+
+		if (pokemonName == '') {
+			pokemonName = prompt('Please type at least one letter for your pokemon', ''); 
+			if (pokemonName == null) {
+				return;
+			}
+
+		} 
+		if (pokemonName.indexOf(' ') != -1) {
+			pokemonName = prompt('Please type a name with no spaces', ''); 
+			if (pokemonName == null) {
+				return;
+			}
+
+		}
+		if (pokemonName.match(/[0-9]/) != null) {
+			pokemonName = prompt('Please only enter letters for your pokemon',''); 
+			if (pokemonName == null) {
+				return;
+			}
+
+		} 
+	} 
+	return pokemonName; 
+}; 
+
+
+function enterPokemonHeight() { 
+	var pokemonHeight = null;
+	while (pokemonHeight == null || pokemonHeight == '' || typeof pokemonHeight != 'number') { 
+		if (pokemonHeight == null) {
+			pokemonHeight = prompt('Please enter a number for your pokemon\'s height',''); 
+			if (pokemonHeight == null) {
+				return;
+			}
+		}
+
+		if (pokemonHeight == '') {
+			pokemonHeight = prompt('Please type at least one number for your pokemon\'s height',''); 
+			if (pokemonHeight == null) {
+				return;
+			}
+
+		}
+		if (pokemonHeight.match(/^\d+(\.?\d*)?$/) == null) {
+			pokemonHeight = prompt('Please only enter numbers for your pokemon\'s height',''); 
+			if (pokemonHeight == null) {
+				return;
+			}
+
+		} 
+		else {
+			pokemonHeight = parseFloat(pokemonHeight);
+		} 
+	} 	
+
+	pokemonHeight += ' m'; 
+
+	return pokemonHeight;
+}; 
+
+function enterPokemonType() {
+	var pokemonType = null; 
+	while (pokemonType == null || pokemonType == '' || typeof pokemonType != 'string' || pokemonType.indexOf(' ') != -1 || pokemonType.match(/[0-9]/) != null) { 
+		if (pokemonType == null) {
+			pokemonType = prompt('Please enter a type for your pokemon',''); 
+			if (pokemonType == null) {
+				return;
+			} 
+		}
+
+		if (pokemonType == '') {
+			pokemonType = prompt('Please type at least one letter for your pokemon\'s type',''); 
+			if (pokemonType == null) {
+				return;
+			}
+
+		} 
+		if (pokemonType.indexOf(' ') != -1) {
+			pokemonType = prompt('Please type a name with no spaces', ''); 
+			if (pokemonType == null) {
+				return;
+			}
+
+		}
+		if (pokemonType.match(/[0-9]/) != null) {
+			pokemonType = prompt('Please only enter letters for your pokemon\'s type',''); 
+			if (pokemonType == null) {
+				return;
+			}
+
+		}  
+
+	} 
+
+	return pokemonType;
+}; 
+
+function anotherType(pokemon, firstType){
+	var anotherType = confirm('Would you like to add another type?') 
+		if (!anotherType){ 
+			pokemon.type = firstType;
+		} 
+		else { 
+			pokemon.type[0] = firstType;
+			while (anotherType){
+				var nextType = enterPokemonType(); 
+				pokemon.type.push(nextType); 
+				anotherType = confirm('Would you like to add another type?');
+			}
+		}
+}
