5d1ff4d6c5d5738eeaa19d3fc03272d6d1c97d97
Hunter Evanoff <hunter547@gmail.com>
2019-10-18 23:20:25 -0400

    Fixed bugs with validation and added a function to redraw the HTML.
    Added more code to fix validation issues. Made a function that will draw the HTML page with the contents of th pokemon array and button.

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ var pokemonRepository = (function () {
 		height: '1.5 m', 
 		type: ['ghost', 'poison']
 	}
-	];
+	]; 
 
 	function add(pokemon) {
 		repository.push(pokemon); // Add new pokemon to the repository
@@ -46,12 +46,11 @@ var pokemonRepository = (function () {
 			var displayString = '';  
 			displayString = '<br />' + pokemon.name+'. '+getTypes(pokemon)+' Height: '+pokemon.height+'.'; 
 			if (parseFloat(pokemon.height) > 1.0) {
-				displayString += ' - Wow, that is tall!'; 
+				displayString = displayString.substring(0, displayString.length-1) + ' - Wow, that is tall!'; 
 			} 
 			document.write(displayString);
 		});
 	}
-
 	return {
 		add: add,
 		getAll: getAll, 
@@ -66,9 +65,7 @@ var newPokemon = {
 	height: '0.6 m', 
 	type: 'normal'
 }; 
-
 pokemonRepository.add(newPokemon);
-
 fullRepository.forEach((currentPokemon) => { 
 	printArrayDetails(currentPokemon);
 }); 
@@ -96,19 +93,21 @@ function getTypes(pokemon) {
 	return typeString;
 };  
 
-
 function enterPokemon(){ 
 	document.body.innerHTML = '';
 	var pokemonName = enterPokemonName(); 
-	if (pokemonName == null) {
+	if (pokemonName == null) { 
+		drawDocument();
 		return;
 	}
 	var pokemonHeight = enterPokemonHeight(); 
-	if (pokemonHeight == null) {
+	if (pokemonHeight == null) { 
+		drawDocument();
 		return;
 	}
 	var pokemonType = enterPokemonType(); 
-	if (pokemonType == null) {
+	if (pokemonType == null) { 
+		drawDocument();
 		return;
 	}
 	var newPokemon = { 
@@ -116,60 +115,55 @@ function enterPokemon(){
 		height: null, 
 		type: [null]
 	};  
-
 	newPokemon.name = pokemonName; 
 	newPokemon.height = pokemonHeight; 
 	anotherType(newPokemon, pokemonType); 
-
-	pokemonRepository.add(newPokemon);
-
-	pokemonRepository.printAll(); 
-
-	var addButton = document.createElement('BUTTON'); 
-	var buttonText = document.createTextNode('Add another pokemon');  
-	addButton.onclick = function() {
-		enterPokemon();
-	}
-	addButton.appendChild(buttonText); 
-	document.writeln();  
-	document.body.appendChild(addButton);
+	pokemonRepository.add(newPokemon); 
+	drawDocument();
+	
 }; 
 
 function enterPokemonName() { 
 	var pokemonName = null; 
-	while (pokemonName == null || pokemonName == '' || typeof pokemonName != 'string' || pokemonName.indexOf(' ') != -1 || pokemonName.match(/[0-9]/) != null) { 
+	while (pokemonName == null 
+	|| pokemonName == ''
+	|| typeof pokemonName != 'string'
+	|| pokemonName.indexOf(' ') != -1
+	|| pokemonName.match(/[0-9]/) != null
+	|| pokemonName.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/) != null) { 
 		if (pokemonName == null) {
 			pokemonName = prompt('Please enter a name for your pokemon', ''); 
 			if (pokemonName == null) {
 				return;
 			}
 		}
-
 		if (pokemonName == '') {
 			pokemonName = prompt('Please type at least one letter for your pokemon', ''); 
 			if (pokemonName == null) {
 				return;
 			}
-
 		} 
 		if (pokemonName.indexOf(' ') != -1) {
 			pokemonName = prompt('Please type a name with no spaces', ''); 
 			if (pokemonName == null) {
 				return;
 			}
-
 		}
 		if (pokemonName.match(/[0-9]/) != null) {
 			pokemonName = prompt('Please only enter letters for your pokemon',''); 
 			if (pokemonName == null) {
 				return;
 			}
-
-		} 
+		}  
+		if (pokemonName.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/) != null) {
+			pokemonName = prompt('Please only enter letters with no special characters'); 
+			if (pokemonName == null) {
+				return;
+			}
+		}
 	} 
-	return pokemonName; 
+	return pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1).toLowerCase();
 }; 
-
 
 function enterPokemonHeight() { 
 	var pokemonHeight = null;
@@ -180,79 +174,97 @@ function enterPokemonHeight() {
 				return;
 			}
 		}
-
 		if (pokemonHeight == '') {
 			pokemonHeight = prompt('Please type at least one number for your pokemon\'s height',''); 
 			if (pokemonHeight == null) {
 				return;
 			}
-
 		}
-		if (pokemonHeight.match(/^\d+(\.?\d*)?$/) == null) {
+		if (pokemonHeight.match(/^\d+(\.?\d*)?$/) == null && pokemonHeight.match(/^\.{1}\d*$/) == null) {
 			pokemonHeight = prompt('Please only enter numbers for your pokemon\'s height',''); 
 			if (pokemonHeight == null) {
 				return;
 			}
-
 		} 
 		else {
 			pokemonHeight = parseFloat(pokemonHeight);
 		} 
 	} 	
-
 	pokemonHeight += ' m'; 
-
 	return pokemonHeight;
 }; 
 
 function enterPokemonType() {
 	var pokemonType = null; 
-	while (pokemonType == null || pokemonType == '' || typeof pokemonType != 'string' || pokemonType.indexOf(' ') != -1 || pokemonType.match(/[0-9]/) != null) { 
+	while (pokemonType == null 
+	|| pokemonType == '' 
+	|| typeof pokemonType != 'string' 
+	|| pokemonType.indexOf(' ') != -1 
+	|| pokemonType.match(/[0-9]/) != null 
+	|| pokemonType.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/) != null) { 
 		if (pokemonType == null) {
 			pokemonType = prompt('Please enter a type for your pokemon',''); 
 			if (pokemonType == null) {
 				return;
 			} 
 		}
-
 		if (pokemonType == '') {
 			pokemonType = prompt('Please type at least one letter for your pokemon\'s type',''); 
 			if (pokemonType == null) {
 				return;
 			}
-
 		} 
 		if (pokemonType.indexOf(' ') != -1) {
 			pokemonType = prompt('Please type a name with no spaces', ''); 
 			if (pokemonType == null) {
 				return;
 			}
-
 		}
 		if (pokemonType.match(/[0-9]/) != null) {
 			pokemonType = prompt('Please only enter letters for your pokemon\'s type',''); 
 			if (pokemonType == null) {
 				return;
 			}
-
+		} 
+		if (pokemonType.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/) != null) {
+			pokemonType = prompt('Please only enter letters with no special characters',''); 
+			if (pokemonType == null) {
+				return;
+			}
 		}  
-
 	} 
-
 	return pokemonType;
 }; 
 
 function anotherType(pokemon, firstType){
 	var anotherType = confirm('Would you like to add another type?') 
 		if (!anotherType){ 
-			pokemon.type = firstType;
+			pokemon.type = firstType.charAt(0).toUpperCase() + firstType.slice(1).toLowerCase();
 		} 
 		else { 
-			pokemon.type[0] = firstType;
+			pokemon.type[0] = firstType.charAt(0).toUpperCase() + firstType.slice(1).toLowerCase();
 			while (anotherType){
 				var nextType = enterPokemonType(); 
-				pokemon.type.push(nextType); 
+				if (nextType == null) {
+					break;
+				}
+				if (typeof nextType != 'undefined' ) {
+					pokemon.type.push(nextType.toLowerCase()); 
+				}	
 				anotherType = confirm('Would you like to add another type?');
 			}
 		}
+}; 
+
+function drawDocument() {
+	pokemonRepository.printAll(); 
+	var addButton = document.createElement('BUTTON'); 
+	var buttonText = document.createTextNode('Add another pokemon');  
+	addButton.onclick = function() {
+		enterPokemon();
+	}
+	addButton.appendChild(buttonText); 
+	document.write('<br />');  
+	document.body.appendChild(addButton); 
+	document.title = 'Simple JS App';
 }
