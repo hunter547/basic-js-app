ddefd35d7fd52d524879bcc7d7cec8d6653d9901
hunter547 <hunter547@gmail.com>
2019-10-23 16:15:27 -0400

    Added support for modal messages
    Added functionality that displays a modal message when the user clicks on a specific pokemon. Still working on the design of the message shown.

--- js/script.js
+++ js/script.js
@@ -49,8 +49,8 @@ var pokemonRepository = (function () {
 
 	// A simple function that logs the contents of the pokemon objects to the console.
 	function showDetails(pokemon) {
-		loadDetails(pokemon).then(function () {
-    		console.log(pokemon);   
+		loadDetails(pokemon).then(function () { 
+			showModal(pokemon);   
     	});
 	} 
 
@@ -58,15 +58,100 @@ var pokemonRepository = (function () {
 		var url = item.detailsUrl;
 		return fetch(url).then(function (response) {
 			return response.json();
-		}).then(function (details) {
+		}).then(function (details) { 
 	       // Now we add the details to the item
 	       item.imageUrl = details.sprites.front_default;
 	       item.height = details.height;
-	       item.types = Object.keys(details.types);
+	       item.types = details.types;
   		}).catch(function (e) {
   		   console.error(e);
   		});
-	}
+	}  
+
+
+	// MODALS
+
+	function showModal(pokemon) {
+		var $modalContainer = document.querySelector('#modal-container'); 
+		// Clear any existing text if being used again. 
+		$modalContainer.innerHTML = ''; 
+
+		// Will close the modal container if the user clicks out side of the modal div 
+		$modalContainer.addEventListener('click', (e) => {
+		 	var target = e.target;
+		  	if (target === $modalContainer) {
+		  	hideModal();
+			}
+		}); 
+
+		// Create a modal div to go into the container 
+		var modal = document.createElement('div'); 
+		modal.classList.add('modal'); 
+
+		// Give the modal some content based on the pokemon and UI elements  
+		var closeButton = document.createElement('BUTTON'); 
+		closeButton.classList.add('modal-close'); 
+		closeButton.innerText = 'Close'; 
+		closeButton.addEventListener('click', hideModal);
+
+		var modalTitle = document.createElement('H1'); 
+		modalTitle.innerText = pokemon.name; 
+
+		var modalBodyDiv = document.createElement('div'); 
+		modalBodyDiv.classList.add('modal-body-div');
+
+		var modalBody = document.createElement('P'); 
+		var modalBodyTextDiv = document.createElement('div'); 
+		modalBodyTextDiv.classList.add('modal-body-text-div');
+		modalBodyTextDiv.appendChild(modalBody);
+		modalBody.innerText = 'Height: ' + (pokemon.height / 10) + ' m';
+
+		var modalTypeTable = document.createElement('table'); 
+		modalTypeTable.classList.add('types-table');
+		var modalTypeTblHead = document.createElement('th');
+		modalTypeTblHead.innerText = 'Type';
+		if (pokemon.types.length > 1) {
+			modalTypeTblHead.innerText = 'Types';
+		} 
+
+		modalTypeTable.appendChild(modalTypeTblHead); 
+
+		pokemon.types.forEach((type) => {
+			var modalTableContent = document.createElement('td'); 
+			modalTableContent.innerText = type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1); 
+			modalTableContent.classList.add('types-content');
+			modalTypeTblHead.appendChild(modalTableContent);
+		}); 
+
+		var modalImage = document.createElement('IMG'); 
+		modalImage.src = pokemon.imageUrl;
+
+		modal.appendChild(closeButton); 
+		modal.appendChild(modalTitle);
+		modalBodyDiv.appendChild(modalImage);
+		modalBodyDiv.appendChild(modalBodyTextDiv); 
+		modalBodyDiv.appendChild(modalTypeTable); 
+		modal.appendChild(modalBodyDiv);
+		
+
+		$modalContainer.appendChild(modal);
+
+		// Display the modal after all content has been set 
+		$modalContainer.classList.add('is-visible'); 
+	}  
+
+
+	function hideModal() {
+		document.querySelector('#modal-container').classList.remove('is-visible');
+	} 
+
+	// Add a listener that will close the modal if the user presses the escape button
+	window.addEventListener('keydown', (e) => {
+		var $modalContainer = document.querySelector('#modal-container');
+		if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
+			hideModal();  
+		}
+	});
 
 	return {
 		add: add,
